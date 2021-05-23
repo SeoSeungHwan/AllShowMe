891fb7afe1161009e20b2bab2726bebb55e65de8
DESKTOP-RE5PC2N\seunghwan <tmdghks3264@naver.com>
2021-05-23 16:34:51 +0900

    Implement the webview loading progress bar

--- app/src/main/java/com/borrow/allshowme/MainActivity.kt
+++ app/src/main/java/com/borrow/allshowme/MainActivity.kt
@@ -1,34 +1,67 @@
 package com.borrow.allshowme
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_web_view.*
 
 
 private const val NUM_PAGES = 5
 
 class MainActivity : FragmentActivity() {
     private lateinit var mPager: ViewPager
-
+    private var keyWord : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Instantiate a ViewPager and a PagerAdapter.
+        //tabLayout과 ViewPager 연동
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.addTab(tabLayout.newTab().setText("네이버"));
+        tabLayout.addTab(tabLayout.newTab().setText("쿠팡"));
+        tabLayout.addTab(tabLayout.newTab().setText("티몬"));
+        tabLayout.addTab(tabLayout.newTab().setText("G마켓"));
+        tabLayout.addTab(tabLayout.newTab().setText("옥션"));
+
+        // ViewPager 움직임 설정
         viewPager.setPageTransformer(true, ZoomOutPageTransformer())
-        // The pager adapter, which provides the pages to the view pager widget.
-        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
-        viewPager.adapter = pagerAdapter
+        // ViewPager Adapter 설정
+        val adapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        viewPager.adapter = adapter
 
 
         searchView.setOnClickListener {
             searchView.isIconified = false
         }
+
+        //검색완료 시 키워드 저장 후 브라우저 호출하기
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                keyWord = query
+                (viewPager.adapter as ScreenSlidePagerAdapter).notifyDataSetChanged()
+                return false
+            }
+        })
+
+        //TODO 공유하기 클릭시 현재 사이트 주소 알아내기
+        share_btn.setOnClickListener {
+            /*val sharingIntent = Intent(Intent.ACTION_SEND)
+            sharingIntent.type = "text/html"
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, "haha")
+            startActivity(Intent.createChooser(sharingIntent, "Share using text"))*/
+        }
+
     }
 
     override fun onBackPressed() {
@@ -44,9 +77,49 @@ class MainActivity : FragmentActivity() {
 
 
     private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
         override fun getCount(): Int = NUM_PAGES
 
-        override fun getItem(position: Int): Fragment = WebViewFragment()
+        override fun getPageTitle(position: Int): CharSequence? {
+            when(position){
+                0 -> return "네이버"
+                1 -> return "쿠팡"
+                2 -> return "티몬"
+                3 -> return "G마켓"
+                4 -> return "옥션"
+            }
+            return ""
+        }
+        override fun getItem(position: Int): Fragment {
+            val fragment = WebViewFragment()
+            val bundle =Bundle(1)
+            when(position){
+                0 -> bundle.putString(
+                    "url",
+                    "https://msearch.shopping.naver.com/search/all?query=" + keyWord
+                )
+                1 -> bundle.putString("url", " https://m.coupang.com/nm/search?q=" + keyWord)
+                2 -> bundle.putString(
+                    "url",
+                    " http://m.search.tmon.co.kr/search?#keyword=" + keyWord
+                )
+                3 -> bundle.putString(
+                    "url",
+                    " http://browse.gmarket.co.kr/search?keyword=" + keyWord
+                )
+                4 -> bundle.putString(
+                    "url",
+                    " https://browse.auction.co.kr/search?keyword=" + keyWord
+                )
+            }
+            fragment.arguments = bundle
+            return fragment
+        }
+
+        //ViewPager 새로고침 구현
+        override fun getItemPosition(`object`: Any): Int {
+            return POSITION_NONE
+        }
     }
 
 
--- app/src/main/java/com/borrow/allshowme/WebViewFragment.kt
+++ app/src/main/java/com/borrow/allshowme/WebViewFragment.kt
@@ -1,12 +1,17 @@
 package com.borrow.allshowme
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fragment_web_view.*
 import kotlinx.android.synthetic.main.fragment_web_view.view.*
 
@@ -22,20 +27,35 @@ class WebViewFragment : Fragment() {
         //webView 세팅
         root.webView.webViewClient = WebViewClient()
         var mWebViewSetting = root.webView.settings
-        mWebViewSetting.javaScriptEnabled =true
-        mWebViewSetting.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
-        mWebViewSetting.setSupportMultipleWindows(false); // 윈도우 여러개 사용여부
-        mWebViewSetting.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
-        mWebViewSetting.useWideViewPort = true; // wide viewport 사용 여부
-        mWebViewSetting.setSupportZoom(true); // Zoom사용여부
-        mWebViewSetting.javaScriptCanOpenWindowsAutomatically = false; // 자바스크립트가 window.open()사용할수있는지 여부
-        mWebViewSetting.loadWithOverviewMode = true; // 메타태그 허용 여부
-        mWebViewSetting.builtInZoomControls = false; // 화면 확대 축소 허용 여부
-        mWebViewSetting.domStorageEnabled = true; // 로컬저장소 허용 여부
-        root.webView.loadUrl("https://www.naver.com/"); // 웹뷰 사이트 주소 및 시작
+        mWebViewSetting?.javaScriptEnabled =true
+        mWebViewSetting?.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+        mWebViewSetting?.setSupportMultipleWindows(false); // 윈도우 여러개 사용여부
+        mWebViewSetting?.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+        mWebViewSetting?.useWideViewPort = true; // wide viewport 사용 여부
+        mWebViewSetting?.setSupportZoom(true); // Zoom사용여부
+        mWebViewSetting?.javaScriptCanOpenWindowsAutomatically = false; // 자바스크립트가 window.open()사용할수있는지 여부
+        mWebViewSetting?.loadWithOverviewMode = true; // 메타태그 허용 여부
+        mWebViewSetting?.builtInZoomControls = false; // 화면 확대 축소 허용 여부
+        mWebViewSetting?.domStorageEnabled = true; // 로컬저장소 허용 여부
+        arguments?.getString("url")?.let { root.webView.loadUrl(it) }; // 웹뷰 사이트 주소 및 시작
+
+        //로딩 ProgressBar구현
+        root.webView.webChromeClient = object : WebChromeClient(){
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                if(newProgress==100){
+                    root.progressBar.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+
+
+
+
+
+
         return root
-
-
     }
+
 
 }
